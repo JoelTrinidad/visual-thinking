614b520b7e07bc4788b8a89914966d7ed7637dcc
Joel Trinidad <jth_2929@hotmail.com>
2022-07-05 12:48:29 -0500

    proyect initialized

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        no-var: "error",
+        "no-unused-vars": ["error", { "vars": "all", "args": "none" }],
+    }
+}
